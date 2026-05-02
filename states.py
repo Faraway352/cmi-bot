db23e21b2420e08ce4d99f66f9f98065febd7b5d
Faray <danisoveg@gmail.com>
2026-05-02 18:24:23 +0400

    Добавлены состояния редактирования профиля

--- states.py
+++ states.py
@@ -2,6 +2,13 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Registration(StatesGroup):
     waiting_for_phone = State()
-    waiting_for_full_name = State()      # вместо имени и фамилии – одно поле
+    waiting_for_full_name = State()
     waiting_for_gender = State()
-    waiting_for_birthday = State()       # переименовано
+    waiting_for_birthday = State()
+
+# Состояния для личного кабинета (редактирование отдельных полей)
+class ProfileEdit(StatesGroup):
+    waiting_for_full_name = State()
+    waiting_for_phone = State()
+    waiting_for_gender = State()
+    waiting_for_birthday = State()
