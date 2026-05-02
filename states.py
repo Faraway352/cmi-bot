a1b07cb6c0c0c2badc333f823c8b13c8f4540517
Faraway352 <danisoveg@gmail.com>
2026-05-02 04:37:38 +0400

    Update states.py

--- states.py
+++ states.py
@@ -2,7 +2,6 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Registration(StatesGroup):
     waiting_for_phone = State()
-    waiting_for_first_name = State()
-    waiting_for_last_name = State()
+    waiting_for_full_name = State()      # вместо имени и фамилии – одно поле
     waiting_for_gender = State()
-    waiting_for_birth_date = State()   # вместо waiting_for_age
+    waiting_for_birthday = State()       # переименовано
