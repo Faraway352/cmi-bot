6daac3876e94e39e4840b90f6475f4414d9d237d
Faray <danisoveg@gmail.com>
2026-05-02 20:27:04 +0400

    Update states.py

--- states.py
+++ states.py
@@ -16,4 +16,22 @@ class ProfileEdit(StatesGroup):
     waiting_for_email = State()
 
 class FeedbackFlow(StatesGroup):
-    waiting_for_text = State()   # ожидание текста отзыва
+    waiting_for_text = State()
+
+# ---------- Админка ----------
+class AdminEventCreate(StatesGroup):
+    waiting_for_title = State()
+    waiting_for_description = State()
+    waiting_for_date = State()
+    waiting_for_time = State()
+    waiting_for_location = State()
+    waiting_for_limit = State()
+    waiting_for_is_paid = State()
+
+class AdminEventEdit(StatesGroup):
+    choose_field = State()
+    waiting_for_value = State()
+
+class AdminBroadcast(StatesGroup):
+    waiting_for_message = State()
+    confirm = State()
