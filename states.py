d644eb187f47c6670fca57f1a0c0802cc24cfbaf
ravilkudzhaev14-cmyk <ravilkudzhaev14@gmail.com>
2026-06-15 23:01:18 +0300

    states.py

--- states.py
+++ states.py
@@ -5,6 +5,9 @@ class Registration(StatesGroup):
     waiting_for_full_name = State()
     waiting_for_gender = State()
     waiting_for_birthday = State()
+    waiting_for_tg_username = State()
+    waiting_for_vk = State()
+    waiting_for_email = State()
 
 class ProfileEdit(StatesGroup):
     waiting_for_full_name = State()
